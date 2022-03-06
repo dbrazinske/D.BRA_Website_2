6d3c482308a8c829cab7d9f45bbcdb17a73bf911
dbrazinske <dovilebrazinkse@gmail.com>
2022-03-06 16:40:14 +0100

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var anchors = document.getElementsByClassName("link");
+var burger = document.getElementById("burger");
+
+// Toggles links style
+for(var x of anchors){
+    x.addEventListener("click", function(){
+
+        for(var x of anchors){
+            x.classList.remove("selected");
+        }
+        this.classList.add("selected");
+    })
+}
+
+// Toggle links display and burger style
+burger.addEventListener("click", function(){
+    for(var x of anchors){
+        x.classList.toggle("block");
+        x.classList.remove("selected");
+    }
+    this.classList.toggle("rotate");
+});
+
+// Change on resize
+window.addEventListener("resize", function(){
+     for(var x of anchors){
+        x.classList.remove("block");
+        x.classList.remove("selected");
+    }
+    burger.classList.remove("rotate");
+});

26536fec2d940af482451a806afa0d5ba9f8fc03
dbrazinske <dovilebrazinkse@gmail.com>
2022-03-12 13:27:10 +0100

    init

--- script.js
+++ script.js
@@ -31,3 +31,29 @@ window.addEventListener("resize", function(){
     }
     burger.classList.remove("rotate");
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+      1000: {
+          slidesPerView: 3,
+          slidesPerGroup: 1,
+      }
+  }
+
+  
+});

49c5074069d3c1e4792fa309ed698e51bc63c093
dbrazinske <dovilebrazinkse@gmail.com>
2022-03-13 16:32:56 +0100

    init

--- tabs.js
+++ tabs.js
@@ -1,8 +1,9 @@
 "use strict";
 // Tabs section3
 function changeHeader(event, name){
+    
     var tabs = document.querySelectorAll(".tab");
-    var buttons = document.querySelectorAll("#button1");
+    var buttons = document.querySelectorAll(".button1");
 
     // // Nuima stilius ir display
     for(var i = 0; i < tabs.length; i++){
@@ -12,16 +13,18 @@ function changeHeader(event, name){
     
     document.getElementById(name).style.display = "block";
     event.currentTarget.classList.add("selected");
-
+    
 
     setTimeout(function(){
         var tabs = document.querySelectorAll(".tab");
         for (var x of tabs){
             x.classList.remove("tabAnimation");
         }
-        
         document.getElementById(name).classList.add("tabAnimation");
     }, 10);
+    
 }
 
 changeHeader("pirmas");
+
+

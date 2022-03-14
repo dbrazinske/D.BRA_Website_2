4f1b013f96ffefb3bb0560b2a1f83c74dd620ecd
dbrazinske <dovilebrazinkse@gmail.com>
2022-03-14 18:56:41 +0100

    pavargau

---
+++ nav2.js
@@ -0,0 +1,30 @@
+// // "use strict";
+
+// // var anchors = document.getElementsByClassName("link");
+// // var burger = document.getElementById("burger");
+
+// for(var x of anchors){
+//     x.addEventListener("click", function(){
+
+//         for(var x of anchors){
+//             x.classList.remove("selected");
+//         }
+//         this.classList.add("selected");
+//     })
+// }
+
+// burger.addEventListener("click", function(){
+//     for(var x of anchors){
+//         x.classList.toggle("block");
+//         x.classList.remove("selected");
+//     }
+//     this.classList.toggle("rotate");
+// });
+
+// window.addEventListener("resize", function(){
+//      for(var x of anchors){
+//         x.classList.remove("block");
+//         x.classList.remove("selected");
+//     }
+//     burger.classList.remove("rotate");
+// });

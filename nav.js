6d1af55250f3bd8e383b7c49486c0002db8a38ec
dbrazinske <dovilebrazinkse@gmail.com>
2022-03-14 14:44:12 +0100

    init

--- nav.js
+++ nav.js
@@ -1,21 +1,21 @@
-"use strict";
+// "use strict";
 var sections = document.querySelectorAll(`section`);
 var navLi = document.querySelector(`nav .navContainer ul li`);
 
-window.addEventListener(`scroll`, ()=>{
+window.addEventListener(`scroll`, ()=> {
     let current = ``;
     sections.forEach( section => {
         var sectionTop = section.offsetTop;
         var sectionHeight = section.clientHeight;
         if(pageYOffset >= (sectionTop - sectionHeight / 2)){
-            current = section.getAttribute("id");
+            current = section.getAttribute( `id`);
         } 
-    });
+    })
     console.log(current);
     navLi.forEach( li => {
         li.classList.remove(`active`);
-        // // if(a.classList.contains(current)){
-        // //     listenerCount.classList.add(`active`);
-        // // }
-    });
-});
+        if(li.classList.contains(current)){
+           li.classList.add(`active`)
+        }
+    })
+})
